a64f4efdeaf873b0e77b81dbec58457f9534e347
Animesh Shukla <animeshshukla1518@gmail.com>
2024-04-14 15:23:04 +0530

    fix: Lab2 cpp files fixed for latest gcc compiler

--- Lab2/c++/client.cpp
+++ Lab2/c++/client.cpp
@@ -1,90 +1,73 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+
+#define PORT 8080
+#define SERVER_ADDR "127.0.0.1"
+#define CLIENT_FILES_DIR "./clientFiles/"
 
 using namespace std;
 
-char *getFileName()
-{
-    cout << "Enter the file name : " << endl;
-    char input[1024];
-    cin.getline(input, sizeof(input));
-    char *fileName = input;
-    return fileName;
-}
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock==-1){
+        cerr << "Socket creation error" << endl;
+        close(sock);
+        return -1;
+    }
+    struct sockaddr_in serv_addr;
 
-int main()
-{
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1)
-    {
-        cout << "Error creating client socket" << endl;
-        close(clientSocket);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    serv_addr.sin_port = htons(PORT);
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        cerr << "Connection failed" << endl;
+        close(sock);
         return -1;
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddress.sin_port = htons(5000);
+    string fileName;
+    cout << "Enter file name: ";
+    cin >> fileName;
 
-    if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
-    {
-        cout << "Error connecting to server" << endl;
-        close(clientSocket);
+    send(sock, fileName.c_str(), fileName.length(), 0);
+
+    int fileSize;
+    recv(sock, &fileSize, sizeof(fileSize), 0);
+
+    if (fileSize == -1) {
+        cout << "File not found on server." << endl;
+        close(sock);
         return -1;
     }
 
-    cout << "Connected to server" << endl;
+    string filePath = CLIENT_FILES_DIR + fileName;
+    ofstream outFile(filePath, ios::binary);
 
-    while (true)
-    {
-        char *fileName = getFileName();
-        cout << "fileName" << fileName << endl;
-        ssize_t bytesSend = send(clientSocket, fileName, strlen(fileName), 0);
-        if (bytesSend == -1)
-        {
-            cout << "Error requesting file" << endl;
-            continue;
-        }
-        cout << "File requested : " << fileName << endl;
-        char buff[1024];
-        ssize_t bytesRecieved = recv(clientSocket, buff, sizeof(buff), 0);
-        if (bytesRecieved == -1)
-        {
-            cout << "Error recieving file" << endl;
-            continue;
-        }
-        if(buff[0]=='-'){
-            cout<<"File is not available at the moment"<<endl;
-            continue;
-        }
-        const char *path = "./clientFiles/";
-        char *fullPath = (char *)malloc(strlen(path) + strlen(fileName) + 1);
-        if (fullPath == NULL)
-        {
-            perror("Memory allocation failed");
-            return 1;
-        }
-        strcpy(fullPath, path);
-        strcat(fullPath, fileName);
-        cout << "filePath :" << fullPath << endl;
-        buff[bytesRecieved] = '\0';
-        FILE *fd = fopen(fullPath, "wb");
-        if (fd == NULL)
-        {
-            cout << "Error opening file" << endl;
-        }
-        else
-        {
-            fwrite(&buff, 1, bytesRecieved, fd);
-            fclose(fd);
-        }
+    if (!outFile.is_open()) {
+        cerr << "Failed to create file" << endl;
+        close(sock);
+        return -1;
     }
 
-    close(clientSocket);
+    int bytesRead;
+    char fileBuffer[1024];
+    while (fileSize > 0) {
+        bytesRead = recv(sock, fileBuffer, sizeof(fileBuffer), 0);
+        outFile.write(fileBuffer, bytesRead);
+        fileSize -= bytesRead;
+    }
+
+    outFile.close();
+    cout << "File received successfully." << endl;
+
+    close(sock);
+
+    return 0;
 }
--- Lab2/c++/server.cpp
+++ Lab2/c++/server.cpp
@@ -1,99 +1,93 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <cstdlib>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
-#include <filesystem>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+
+
+#define SERVER_ADDR "127.0.0.1"
+#define PORT 8080
+#define SERVER_FILES_DIR "./serverFiles/"
 
 using namespace std;
-namespace fs = filesystem;
-string directory = "./serverFiles/";
 
-int main(){
-    int serverSocket = socket(AF_INET,SOCK_STREAM,0);
-    if(serverSocket==-1){
-        cout<<"Error creating server"<<endl;
-        close(serverSocket);
+int main() {
+    int server_socket=socket(AF_INET, SOCK_STREAM, 0);
+    if(server_socket==-1){
+        cerr << "Socket creation error" << endl;
+        close(server_socket);
+        return -1;
+    }
+    int new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    address.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        cerr << "Bind failed" << endl;
+        close(server_socket);
         return -1;
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family=AF_INET;
-    serverAddress.sin_addr.s_addr=inet_addr("127.0.0.1");
-    serverAddress.sin_port=htons(5000);
-
-    if(bind(serverSocket,(struct sockaddr*)&serverAddress,sizeof(serverAddress))==-1){
-        cout<<"Error binding socket to ip"<<endl;
-        close(serverSocket);
+    if (listen(server_socket, 5) < 0) {
+        cerr << "Listen failed" << endl;
+        close(server_socket);
         return -1;
     }
-    
-    if(listen(serverSocket,10)==-1){
-        cout<<"Error listening server socket"<<endl;
-        close(serverSocket);
-        return -1;
-    }
+    cout<<"Hey there!! , Server listening on 127.0.0.1:8080"<<endl;
 
-    cout<<"Server listening on 127.0.0.1:5000"<<endl;
+    while (true) {
+        if ((new_socket = accept(server_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            cerr << "Accept failed" << endl;
+            close(server_socket);
+            return -1;
+        }
 
-    sockaddr_in clientAddress;
-    socklen_t clientAddressLen = sizeof(clientAddress);
-    int clientSocket = accept(serverSocket,(struct sockaddr*)&clientSocket,&clientAddressLen);
-    if(clientSocket==-1){
-        cout<<"Error creating client socket"<<endl;
-        close(serverSocket);
-        close(clientSocket);
-        return -1;
-    }
-
-    cout<<"Connection accepted from "<<inet_ntoa(clientAddress.sin_addr)<<endl;
-
-
-    while(true){
-        char buff[1024];
-        ssize_t bytesRecieved = recv(clientSocket,buff,sizeof(buff),0);
-        if(bytesRecieved==-1){
-            cout<<"Error recieving fileName"<<endl;
+        int valread = read(new_socket, buffer, 1024);
+        if (valread <= 0) {
+            close(new_socket);
             continue;
         }
-        buff[bytesRecieved]='\0';
-        cout<<"File requested : "<<buff<<endl;
-        string filePath = directory+buff;
-        if(fs::exists(filePath)){
-            ifstream inputFile(filePath,ios::binary);
-            if(inputFile.is_open()){
-                inputFile.seekg(0,ios::end);
-                streampos fileSize=inputFile.tellg();
-                inputFile.seekg(0,ios::beg);
 
-                char *fileContent = new char[static_cast<size_t>(fileSize)];
+        string filePath = SERVER_FILES_DIR + string(buffer);
 
-                inputFile.read(fileContent,fileSize);
-                inputFile.close();
+        ifstream file(filePath, ios::binary);
+        if (!file.is_open()) {
+            cout << "File not found: " << filePath << endl;
 
-                ssize_t bytesSend = send(clientSocket,fileContent,fileSize,0);
-                if(bytesSend==-1){
-                    cout<<"Error sending file"<<endl;
-                }
-                else{
-                    cout<<"File sent successfully"<<endl;
-                }
+            int notFound = -1;
+            send(new_socket, &notFound, sizeof(notFound), 0);
 
-                delete [] fileContent;
-            }
+            close(new_socket);
+            continue;
         }
-        else{
-            char message[1024] = "-1";
-            ssize_t bytesSend = send(clientSocket,message,strlen(message),0);
-            if(bytesSend==-1){
-                cout<<"Error sending response message"<<endl;
-            }
+
+        struct stat fileStat;
+        if (stat(filePath.c_str(), &fileStat) < 0) {
+            cout << "Error getting file size." << endl;
+            close(new_socket);
+            continue;
         }
+
+        int fileSize = fileStat.st_size;
+        send(new_socket, &fileSize, sizeof(fileSize), 0);
+
+        char fileBuffer[1024];
+        while (!file.eof()) {
+            file.read(fileBuffer, sizeof(fileBuffer));
+            send(new_socket, fileBuffer, file.gcount(), 0);
+        }
+
+        file.close();
+        close(new_socket);
     }
 
-    close(serverSocket);
-    close(clientSocket);
-
+    return 0;
 }
